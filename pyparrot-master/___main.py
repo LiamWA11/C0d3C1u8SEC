7294e03a63d7e3e8c3067dfd3f3fe9016536e6c2
r3dh0t <35854972+r3dh0t@users.noreply.github.com>
2018-04-23 16:42:21 +1000

    remove stuff

--- pyparrot-master/___main.py
+++ pyparrot-master/___main.py
@@ -22,29 +22,12 @@ try:
 	    print("taking off!")
 	    mambo.safe_takeoff(5)
 
-	    print("Flying direct: going forward (positive pitch)")
-	    mambo.fly_direct(roll=0, pitch=50, yaw=0, vertical_movement=0, duration=1)
-
 	    print("Showing turning (in place) using turn_degrees")
 	    mambo.turn_degrees(90)
 	    mambo.smart_sleep(2)
 	    mambo.turn_degrees(-90)
 	    mambo.smart_sleep(2)
 
-	    print("Flying direct: yaw")
-	    mambo.fly_direct(roll=0, pitch=0, yaw=50, vertical_movement=0, duration=1)
-
-	    print("Flying direct: going backwards (negative pitch)")
-	    mambo.fly_direct(roll=0, pitch=-50, yaw=0, vertical_movement=0, duration=0.5)
-
-	    print("Flying direct: roll")
-	    mambo.fly_direct(roll=50, pitch=0, yaw=0, vertical_movement=0, duration=1)
-
-	    print("Flying direct: going up")
-	    mambo.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=50, duration=1)
-
-	    print("Flying direct: going around in a circle (yes you can mix roll, pitch, yaw in one command!)")
-	    mambo.fly_direct(roll=25, pitch=0, yaw=50, vertical_movement=0, duration=3)
 
 	    print("landing")
 	    mambo.safe_land(5)
