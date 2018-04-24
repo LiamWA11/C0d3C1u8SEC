7dda774b6158d7e2f5010d7e29ea17a83409a363
Ben Elliott <Benjaminelliott2106@gmail.com>
2018-04-24 10:18:00 +1000

    Cleaning
    Cleaned up  a bit of stuff, added some comments

--- pyparrot-master/___main.py
+++ pyparrot-master/___main.py
@@ -16,38 +16,37 @@ print("connected: %s" % success)
 try:
 
 	if (success):
-	    # get the state information
-	    print("sleeping")
-	    mambo.smart_sleep(2)
-	    mambo.ask_for_state_update()
-	    mambo.smart_sleep(2)
+		# get the state information
+		print("sleeping")
+		mambo.smart_sleep(2)
+		mambo.ask_for_state_update()
+		mambo.smart_sleep(2)
 
-	    print("taking off!")
-	    mambo.safe_takeoff(5)
+		print("taking off!")
+		mambo.safe_takeoff(5)
 
-	    print("Showing turning (in place) using turn_degrees")
-	    mambo.turn_degrees(90)
-	    mambo.smart_sleep(2)
-	    mambo.turn_degrees(-90)
-	    mambo.smart_sleep(2)
+		#Tell the drone what to do here:
+		print("Showing turning (in place) using turn_degrees")
+		mambo.turn_degrees(90)
+		mambo.smart_sleep(2)
+		mambo.turn_degrees(-90)
+		mambo.smart_sleep(2)
 
+		print("landing")
+		mambo.safe_land(5)
+		mambo.smart_sleep(5)
 
-	    print("landing")
-	    mambo.safe_land(5)
-	    mambo.smart_sleep(5)
-
-	    print("disconnect")
-	    mambo.disconnect()
+		print("disconnect")
+		mambo.disconnect()
 
 except KeyboardInterrupt:
-        print("Failsafe activated")
-        mambo.safe_land(5)
-        mambo.smart_sleep(5)
-        mambo.disconnect()
-
-except Exception as e:
-	print("Error")
+	print("Failsafe activated")
 	mambo.safe_land(5)
 	mambo.smart_sleep(5)
 	mambo.disconnect()
-	print(str(e))
+
+except Exception as e:
+	print("Error: " + str(e))
+	mambo.safe_land(5)
+	mambo.smart_sleep(5)
+	mambo.disconnect()
