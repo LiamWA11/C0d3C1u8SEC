cf22e11f64d428d99c0fb6d57139b62964e7ad32
Scott <scott_letts@hotmail.com>
2018-04-30 15:56:03 +1000

    Pygame Keys
    Added keys for pygame

--- pyparrot-master/__pygameKeyboard.py
+++ pyparrot-master/__pygameKeyboard.py
@@ -12,14 +12,43 @@
 
 import pygame
 import sys
+from Mambo import Mambo
+
+mamboAddr = "e0:14:d0:63:3d:d0"
+
 pygame.init()
 
 pygame.display.set_mode((250, 250))
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                print('Forward')
-            elif event.key == pygame.K_s:
-                print('Backward')
+mambo = Mambo(mamboAddr, use_wifi=True)
+
+print("trying to connect")
+success = mambo.connect(num_retries=3)
+
+try:
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_t:
+                    mambo.safe_takeoff(5)
+                elif event.key == pygame.K_l:
+                    mambo.safe_land(5)
+                elif event.key == pygame.K_w:
+                    mambo.fly_direct(roll=0, pitch=50, yaw=0, vertical_movement=0, duration=0.1)
+                elif event.key == pygame.K_s:
+                    mambo.fly_direct(roll=0, pitch=-50, yaw=0, vertical_movement=0, duration=0.1)
+                elif event.key == pygame.K_a:
+                    mambo.fly_direct(roll=-50, pitch=0, yaw=0, vertical_movement=0, duration=0.1)
+                elif event.key == pygame.K_d:
+                    mambo.fly_direct(roll=50, pitch=0, yaw=0, vertical_movement=0, duration=0.1)
+
+
+except KeyboardInterrupt:
+    print("Keboard Interrupt")
+    mambo.safe_land(5)
+    mambo.smart_sleep(5)
+
+except Exception:
+    print("Error landing")
+    mambo.safe_land(5)
+    mambo.smart_sleep(5)
