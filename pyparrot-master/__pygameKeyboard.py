de3d4b4ed57ce1dc5bbe84dab23aaf4840f03dca
Ben Elliott <Benjaminelliott2106@gmail.com>
2018-05-01 11:58:03 +1000

    Working pygame control, added drone directions to pygame control system
    Fixed ___test_pygame_control_nomambo.py, which would not work doe to the event queue not being handled.
    
    Modified __pygameKeyboard.py by using the working pygame input system in conjuntiuon with drone movement commands.
    
    Added some sweet new ascii art.

--- pyparrot-master/__pygameKeyboard.py
+++ pyparrot-master/__pygameKeyboard.py
@@ -1,62 +1,123 @@
-#########################################
-#                                       #
-#                                       #
-#                                       #
-#                PYGAME                 #
-#                                       #
-#               PYPARROT                #
-#                                       #
-#                                       #
-#                                       #
-#########################################
+"""                 ########################################################################################################
+                     _________ _______  ________  ________ _________  _____________  ______   _________________ _________
+                     \_   ___ \\   _  \ \______ \ \_____  \\_   ___ \/_   |    |   \/  __  \ /   _____/\_____  \\_   ___ \ 
+                     /    \  \//  /_\  \ |    |  \  _(__  </    \  \/ |   |    |   />      < \_____  \   _(__  </    \  \/ 
+                     \     \___\  \_/   \|    `   \/       \     \____|   |    |  //   --   \/        \ /       \     \____
+                      \______  /\_____  /_______  /______  /\______  /|___|______/ \______  /_______  //______  /\______  /
+                             \/       \/        \/       \/        \/                     \/        \/        \/        \/ 
+                    ########################################################################################################
 
+CONTROL SCHEME:
+
+       SPACE : TAKEOFF/LAND
+       W     : FLY FORWARDS
+       A     : SRAFE LEFT
+       S     : STRAFE RIGHT
+       D     : FLY BACKWARDS
+       Z     : ASCEND
+       X     : DESCEND
+       Q     : ROTATE LEFT
+       E     : ROTATE RIGHT
+"""
+
+###IMPORT NECESSARY LIBRARIES
 import pygame
 import sys
 from Mambo import Mambo
 from ___functions import *
 
-mamboAddr = "e0:14:d0:63:3d:d0"
 
-pygame.init()
-
-pygame.display.set_mode((250, 250))
-
+###MAMBO CLASS DEFINITION
+mamboAddr = ""
 mambo = Mambo(mamboAddr, use_wifi=True)
-
-print("trying to connect")
 success = mambo.connect(num_retries=3)
 
-try:
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    if mambo.is_landed():
-                        mambo.takeoff()
-                    else:
-                        mambo.land()
-                elif event.key == pygame.K_w:
-                    mambo.fly_direct(roll=0, pitch=50, yaw=0, vertical_movement=0, duration=0.1)
-                elif event.key == pygame.K_s:
-                    mambo.fly_direct(roll=0, pitch=-50, yaw=0, vertical_movement=0, duration=0.1)
-                elif event.key == pygame.K_a:
-                    mambo.fly_direct(roll=-50, pitch=0, yaw=0, vertical_movement=0, duration=0.1)
-                elif event.key == pygame.K_d:
-                    mambo.fly_direct(roll=50, pitch=0, yaw=0, vertical_movement=0, duration=0.1)
-                elif event.key == pygame.K_z:
-                    mambo.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=-20, duration=0.1)
-                elif event.key == pygame.K_x:
-                    mambo.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=20, duration=0.1)
-                elif event.key == pygame.K_q:
-                    mambo.fly_direct(roll=0, pitch=0, yaw=10, vertical_movement=0, duration=0.1)
-                elif event.key == pygame.K_e:
-                    mambo.fly_direct(roll=0, pitch=0, yaw=-10, vertical_movement=0, duration=0.1)
 
- 
- 
+###DEFINITION OF MAIN PYGAME LOOP FOR CONTROL SYSTEM
+def main():
 
-except KeyboardInterrupt:
-    FailsafeLand(None, mambo)
 
-except Exception as e:
-    FailsafeLand(e, mambo)
+    ###CREATE PYGAME WINDOW AND LOOP EXIT CONDITION
+    pygame.init()
+    screen = pygame.display.set_mode((250, 250))
+    done = 0
+
+
+    try:
+        while not done:
+
+            ###GET STATE OF ALL KEYS
+            keys = pygame.key.get_pressed()
+
+
+            ###CHECK FOR SPECIFIC KEY INPUTS, AND ACT ACCORDINGLY
+            if keys[pygame.K_SPACE]:               #Check for space pressed
+                print("SPACE")
+
+                ###CHECK FOR STATE OF DRONE, AND LAND/TAKEOFF DEPENDING ON CURRENT STATE
+                if mambo.is_landed():
+                    mambo.takeoff()
+                else:
+                    mambo.land()
+
+            if keys[pygame.K_w]:                   #Check for w pressed
+                print("W")
+                ###MOVE FORWARDS
+                mambo.fly_direct(roll=0, pitch=50, yaw=0, vertical_movement=0, duration=0.1)
+
+            if keys[pygame.K_a]:                   #Check for a pressed
+                print("A")
+                ###STRAFE LEFT
+                mambo.fly_direct(roll=-50, pitch=0, yaw=0, vertical_movement=0, duration=0.1)
+
+            if keys[pygame.K_s]:                   #Check for s pressed
+                print("S")
+                ###MOVE BACKWARDS
+                mambo.fly_direct(roll=0, pitch=-50, yaw=0, vertical_movement=0, duration=0.1)
+
+            if keys[pygame.K_d]:                   #Check for d pressed
+                print("D")
+                ###STRAFE LEFTWARDS
+                mambo.fly_direct(roll=50, pitch=0, yaw=0, vertical_movement=0, duration=0.1)
+
+            if keys[pygame.K_z]:                   #Check for z pressed
+                print("Z")
+                ###ASCEND
+                mambo.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=-20, duration=0.1)
+
+            if keys[pygame.K_x]:                   #Check for x pressed
+                print("X")
+                ###DESCEND
+                mambo.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=20, duration=0.1)
+
+            if keys[pygame.K_q]:                   #Check for q pressed
+                print("Q")
+                ###ROTATE ANTI-CLOCKWISE
+                mambo.fly_direct(roll=0, pitch=0, yaw=10, vertical_movement=0, duration=0.1)
+
+            if keys[pygame.K_e]:                   #Check for e pressed
+                print("E")
+                ###ROTATE CLOCKWISE
+                mambo.fly_direct(roll=0, pitch=0, yaw=-10, vertical_movement=0, duration=0.1)
+
+            if keys[pygame.K_ESCAPE]:              #Check for escape pressed, close pygame window
+                print("Shutting Down \n")
+                ###CLOSE GAME WINDOW, LAND THE DRONE
+                FailsafeLand(None, mambo)
+                done = 1
+
+
+            ###HANDLE THE PYGAME EVENT QUEUE, ALLOWING THE PROGRAM TO INTERACT WITH THE REST OF THE OS
+            pygame.event.pump()
+            ###CLEAR THE EVENT QUEUE, AS EVENTS REQUIRE NO FURTHER HANDLING
+            pygame.event.clear()
+
+        pygame.quit()
+
+
+    except Exception as e:
+        FailsafeLand(e, mambo)
+
+
+###RUN MAIN GAME LOOP
+main()
